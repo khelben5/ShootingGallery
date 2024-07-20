fc946fc824b05d39dc67c977c5f6203fb7033ea2
Eduardo Pascua <pascua.eduardo@gmail.com>
2024-07-20 19:36:03 +0200

    Capture mouse events

--- Game1.cs
+++ Game1.cs
@@ -6,8 +6,20 @@ namespace ShootingGallery;
 
 public class Game1 : Game
 {
+    private const int targetRadius = 45;
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
+    private Texture2D targetSprite;
+    private Texture2D crosshairsSprite;
+    private Texture2D backgroundSprite;
+    private SpriteFont gameFont;
+
+    private Vector2 targetPosition = new Vector2(300, 300);
+    private MouseState mouseState;
+    private int score = 0;
+    private bool mouseReleased = true;
 
     public Game1()
     {
@@ -27,7 +39,10 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        targetSprite = Content.Load<Texture2D>("target");
+        crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+        backgroundSprite = Content.Load<Texture2D>("sky");
+        gameFont = Content.Load<SpriteFont>("galleryFont");
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,7 +50,17 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        mouseState = Mouse.GetState();
+        if (mouseState.LeftButton == ButtonState.Pressed && mouseReleased)
+        {
+            score++;
+            mouseReleased = false;
+        }
+
+        if (mouseState.LeftButton == ButtonState.Released)
+        {
+            mouseReleased = true;
+        }
 
         base.Update(gameTime);
     }
@@ -44,7 +69,11 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+        _spriteBatch.DrawString(gameFont, score.ToString(), new Vector2(100, 100), Color.White);
+        _spriteBatch.Draw(targetSprite, targetPosition, Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
